dcfcfe8957bfa989afa6ba327bc5287b2fa982a6
Kevin <kevin@example.com>
2026-04-13 20:54:32 +0800

    fix: ganti ke model yang valid

--- app/action/analizeAction.js
+++ app/action/analizeAction.js
@@ -21,7 +21,7 @@ export async function analizeAction(prevState, formData) {
         }
     }
 
-    const model = "mistralai/mistral-7b-instruct:free"
+    const model = "google/gemini-2.0-flash-001"
     // "openai/gpt-4o-mini"
     // "mistralai/mistral-small-3.1-24b-instruct:free"
 
